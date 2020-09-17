4e6d191433af6694265615cecf4888f14950d9b0
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-09-17 11:26:45 -0300

    Contagem de dias da semana e fim de semana

--- app/controllers/reservas_controller.rb
+++ app/controllers/reservas_controller.rb
@@ -1,5 +1,6 @@
 class ReservasController < ApplicationController
   require 'date'
+  require 'time'
 
   def index
     @reservas = Reserva.all
@@ -11,11 +12,21 @@ class ReservasController < ApplicationController
 
   def new
     @reserva = Reserva.new
-    text = "16Mar2020(mon)"
-    @formatted_date = Date.parse(text).strftime('%d %B %Y (%A)')
-   
+    data1 = "16Mar2020(mon)"
+    data2 = "20Apr2020(mon)"
+    @formatted_data1 = Date.parse(data1).strftime('%d %B %Y %A')
+    @formatted_data2 = Date.parse(data2).strftime('%d %B %Y %A')
+
+    @time_data1 = Time.parse(data1)
+    @time_data2 = Time.parse(data2)
+
+    @differ = (((@time_data2 - @time_data1)/3600)/24).to_s
+    @weekdays = ((Date.parse(data1))..(Date.parse(data2) - 1) ).select {|d| (1..5).include?(d.wday) }.size
+    @weekend_days = (@differ.to_i - @weekdays) 
+
   end
 
+ 
   def update
   end
   
