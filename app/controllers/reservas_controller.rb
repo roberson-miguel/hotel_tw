b6aaa70f0afb0587de5970764157b5d8bf8a9d21
Roberson Miguel <roberson.miguel@biosbug.com.br>
2020-09-18 16:17:05 -0300

    Refatora o calculo de dias

--- app/controllers/reservas_controller.rb
+++ app/controllers/reservas_controller.rb
@@ -29,9 +29,8 @@ class ReservasController < ApplicationController
   end
 
   def calcula_dias_fim_de_semana(differ, weekdays)
-    @weekend_days = (@differ.to_i - @weekdays) 
+    (@differ.to_i - @weekdays) 
   end
-  
 
   def new
     @reserva = Reserva.new
@@ -71,17 +70,8 @@ def set_find
 end
 
 
-# data1 = "16Mar2020(mon)"
-# data2 = "20Apr2020(mon)"
 # @formatted_data1 = Date.parse(data1).strftime('%d %B %Y %A')
 # @formatted_data2 = Date.parse(data2).strftime('%d %B %Y %A')
-
-# @time_data1 = Time.parse(data1)
-# @time_data2 = Time.parse(data2)
-
-# @differ = (((@time_data2 - @time_data1)/3600)/24).to_s
-# @weekdays = ((Date.parse(data1))..(Date.parse(data2) - 1) ).select {|d| (1..5).include?(d.wday) }.size
-# @weekend_days = (@differ.to_i - @weekdays) 
 
 
 #Como usar as data no controller
